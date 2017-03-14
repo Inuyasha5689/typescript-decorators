549c37989cf3694914f83aa415e4d44f7a06a01b
Drake Scott <dascott0204@gmail.com>
2017-03-13 20:39:00 -0500

    created a class decorator

--- app.ts
+++ app.ts
@@ -1,3 +1,13 @@
 /**
  * Created by dasco on 3/9/2017.
  */
+function logged(constructorFn: Function) {
+    console.log(constructorFn);
+}
+
+@logged
+class Person {
+    constructor() {
+        console.log("HI!");
+    }
+}
